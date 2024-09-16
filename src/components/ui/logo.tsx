7461fb844ac6aae7352ecf05f94954915bcef2be
Rumon360 <hmk.rumon@gmail.com>
2024-09-16 21:57:36 +0600

    fixed build

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,11 +1,17 @@
 import { useMousePositionContext } from "../../hooks/useMousePositionContext";
 
 function Logo() {
-  const { cursorSize } = useMousePositionContext();
+  const { cursorHeight, cursorWidth } = useMousePositionContext();
   return (
     <span
-      onMouseEnter={() => cursorSize.set(40)}
-      onMouseLeave={() => cursorSize.set(16)}
+      onMouseEnter={() => {
+        cursorHeight.set(40);
+        cursorWidth.set(40);
+      }}
+      onMouseLeave={() => {
+        cursorHeight.set(16);
+        cursorWidth.set(16);
+      }}
       className="uppercase text-2xl tracking-tighter font-semibold"
     >
       Maker
